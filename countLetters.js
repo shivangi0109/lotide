d8a9b2f00f27d5cd89c2a069522198c3653d535c
Shivangi Srivastava <shi06dec@gmail.com>
2023-05-22 21:19:27 +0000

    Implement countLetters which ake in a sentence (as a string) and then return a count of each of the letters in that sentence

---
+++ countLetters.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} == ${expected}`);
+  } else {
+    console.log(`\uD83D\uDED1 \uD83D\uDED1 \uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  // split the string having spaces, which will convert the string into arrays
+  let newString = string.split(" ");
+  // console.log(newString);
+
+  // to convert back the arrays into string, we need to use join
+  newString = newString.join('');
+  // console.log(newString);
+
+  let results = {};
+
+  for (let char of newString) {
+    // console.log(char);
+    if (results[char]) {
+      results[char] += 1;
+    } else {
+      results[char] = 1;
+    }
+  }
+
+  return results;
+};
+
+const result1 = countLetters('LHL');
+console.log(result1);
+const result2 = countLetters("lighthouse in the house");
+console.log(result2);
+
+assertEqual(result1["L"], 2);
+assertEqual(result1["H"], 1);
+assertEqual(result2["l"], 1);
+assertEqual(result2["h"], 4);
+assertEqual(result2["n"], 2);
