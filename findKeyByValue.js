fe11d9b1d9b00c96b31fa608273cb50c2a1ee771
Shivangi Srivastava <shi06dec@gmail.com>
2023-05-23 00:10:48 +0000

    implement findKeyByValue function which takes in an object and a value and return the first key which contains the given value

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 \u2705 \u2705 Assertion Passed: ${actual} == ${expected}`);
+  } else {
+    console.log(`\uD83D\uDED1 \uD83D\uDED1 \uD83D\uDED1 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Solution 1 - My solution
+const findKeyByValue = function(object, value) {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Solution 2 - Based on the tip
+// const findKeyByValue = function(object, value) {
+//   const keys = Object.keys(object);
+
+//   for (const key of keys) {
+//     if (object[key] === value) {
+//       return key;
+//     }
+//   }
+//   return undefined;
+// };
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
