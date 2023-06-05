d03ccbc09b2f8a812a43cd50a02aee12a9b8dfea
Shivangi Srivastava <shi06dec@gmail.com>
2023-06-05 06:31:27 +0000

    Refactor Lotide - tail by exporting the function by using module.exports

---
+++ test/tailTest.js
@@ -0,0 +1,20 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2:
+const result1 = tail(["Hello"]);
+assertEqual(result1.length, 2); // ensure we get back two elements
+assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 3:
+const result2 = tail([]);
+assertEqual(result2.length, 2); // ensure we get back two elements
+assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
