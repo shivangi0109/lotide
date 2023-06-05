c11c19dcfa540cf042d107cfc8a138612db82d82
Shivangi Srivastava <shi06dec@gmail.com>
2023-06-05 17:53:58 +0000

    incorporate mocha and chai in tailTest.js and middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,27 +1,60 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// For arrays with one or two elements, there is no middle. Return an empty array.
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+describe("#middle", () => {
+  it("returns [5, 6] for [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+  });
 
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+  it("returns [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
 
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [4, 5]
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // => [5, 6]
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
 
-// TEST CASES
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [8, 9]);
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
+
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+
+// // For arrays with one or two elements, there is no middle. Return an empty array.
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+
+// //For arrays with odd number of elements, an array containing a single middle element should be returned.
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+
+// // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8])); // => [4, 5]
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // => [5, 6]
+
+// // TEST CASES
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [5, 6]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [8, 9]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,45 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-// Test Case 2:
-const result1 = tail(["Hello"]);
-assertEqual(result1.length, 2); // ensure we get back two elements
-assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+  it("returns ['4', '3', '7'] for ['1', '4', '3', '7']", () => {
+    assert.deepEqual(tail(['1', '4', '3', '7']), ['4', '3', '7']);
+  });
 
-// Test Case 3:
-const result2 = tail([]);
-assertEqual(result2.length, 2); // ensure we get back two elements
-assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
+  it("returns [5, 7, 4] for [1, 5, 7, 4]", () => {
+    assert.deepEqual(tail([1, 5, 7, 4]), [5, 7, 4]);
+  });
+
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
+
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// // Test Case 1: Check the returned array elements
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 2:
+// const result1 = tail(["Hello"]);
+// assertEqual(result1.length, 2); // ensure we get back two elements
+// assertEqual(result1[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result1[1], "Labs"); // ensure second element is "Labs"
+
+// // Test Case 3:
+// const result2 = tail([]);
+// assertEqual(result2.length, 2); // ensure we get back two elements
+// assertEqual(result2[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result2[1], "Labs"); // ensure second element is "Labs"
